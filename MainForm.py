a88bf5552c749b361fec6d91e921ccb88c24b57b
Prasanna Pakkiam <p.pakkiam@uq.edu.au>
2021-03-09 00:26:41 +1000

    Made the entire form correctly resize (via Tkinter grids) and disable/enable UI elements when toggling between 1D and 2D plots.

--- MainForm.py
+++ MainForm.py
@@ -17,17 +17,75 @@ class MainForm:
         self.root = tk.Tk()
         self.root.wm_title("SQDviz - Data visualisation tool")
 
-        #Left-hand side (plots and the plot-selection)
-        pw_LHS = PanedWindow(orient ='vertical', master=self.root) 
-        pw_LHS.pack(fill=BOTH, expand=1)
-        self.plot_main = self._generate_plot_frame()
-        pw_LHS.add(self.plot_main['frame'],stretch='always')
-        # self.plot_slice = self._generate_plot_frame()
-        # pw_LHS.add(self.plot_slice['frame'])
-        button = tk.Button(master=self.root, text="Quit", command=self._event_quit)
-        button.pack(side=tk.BOTTOM)
-        pw_LHS.add(button)
+        self.frame_LHS = Frame(master=self.root)
 
+        #Left-hand side (plots and the plot-selection)
+        self.pw_plots_main = PanedWindow(orient ='vertical', master=self.frame_LHS)
+        self.plot_main = self._generate_plot_frame()
+        self.pw_plots_main.add(self.plot_main['frame'],stretch='always')
+        self.plot_slice = self._generate_plot_frame()
+        self.pw_plots_main.add(self.plot_slice['frame'])
+        # button = tk.Button(master=self.root, text="Quit", command=self._event_quit)
+        # button.pack(side=tk.BOTTOM)
+        # pw_LHS.add(button)
+
+        self.pw_plots_main.grid(row=0,column=0,sticky='nsew')
+        
+        #########################
+        #PLOT SELECTION CONTROLS#
+        #########################
+        #
+        #Overall frame broken up into columns containing individual frames...
+        self.frame_plot_sel = LabelFrame(master=self.frame_LHS, text = "Plot Parameters")
+        #
+        #######################################
+        #RADIO BUTTONS FOR PLOT TYPE SELECTION#
+        frm_plttyp = Frame(master=self.frame_plot_sel)
+        cur_row = 0
+        #
+        #Labelled frame container with the radio buttons stored in the variable self.plot_dim_type
+        lblfrm_plot_type = LabelFrame(frm_plttyp, text = "Plot Type", padx=10, pady=10)
+        lblfrm_plot_type.grid(row=cur_row,column=0)
+        self.plot_dim_type = tk.IntVar()
+        self.plot_dim_type.set(1)
+        tk.Radiobutton(lblfrm_plot_type, text="1D Plot", padx = 20, variable=self.plot_dim_type, value=1, command=self._event_plotsel_changed).grid(row=0, column=0)
+        tk.Radiobutton(lblfrm_plot_type, text="2D Plot", padx = 20, variable=self.plot_dim_type, value=2, command=self._event_plotsel_changed).grid(row=1, column=0)
+        #
+        frm_plttyp.grid(row=0,column=0)
+        #######################################
+        #
+        #######################################
+        #LISTBOXES FOR AXIS VARIABLE SELECTION#
+        frm_lstbxes = Frame(master=self.frame_plot_sel)
+        rcOff = (0,0) #Axis listbox row-column offset to make it easier to resposition the listboxes
+        #x-Axis column
+        lbl_axis_x = Label(frm_lstbxes, text = "x-axis")
+        lbl_axis_x.grid(row=rcOff[0], column=rcOff[1], padx=10, pady=2)
+        self.lstbx_x = ListBoxScrollBar(frm_lstbxes)
+        self.lstbx_x.frame.grid(row=rcOff[0]+1, column=rcOff[1], padx=10, pady=2)
+        #y-Axis column
+        lbl_axis_y = Label(frm_lstbxes, text = "y-axis")
+        lbl_axis_y.grid(row=rcOff[0], column=rcOff[1]+1, padx=10, pady=2)
+        self.lstbx_y = ListBoxScrollBar(frm_lstbxes)
+        self.lstbx_y.frame.grid(row=rcOff[0]+1, column=rcOff[1]+1, padx=10, pady=2)
+        #
+        frm_lstbxes.rowconfigure(rcOff[0], weight=0)
+        frm_lstbxes.rowconfigure(rcOff[0]+1, weight=1)
+        frm_lstbxes.columnconfigure(rcOff[1], weight=0)
+        frm_lstbxes.columnconfigure(rcOff[1]+1, weight=0)
+        #
+        frm_lstbxes.grid(row=0,column=1,sticky='se')
+        #######################################
+        #
+
+        self.frame_plot_sel.grid(row=1,column=0,sticky='sew')
+
+        self.frame_LHS.rowconfigure(0, weight=1)
+        self.frame_LHS.rowconfigure(1, weight=0)
+        self.frame_LHS.columnconfigure(0, weight=1)
+
+
+        self.frame_LHS.pack(fill=BOTH, expand=1)
 
         self.plot_main['canvas'].mpl_connect("key_press_event", self._event_form_on_key_press)
 
@@ -64,21 +122,36 @@ class MainForm:
         return self.dset[:,0]
 
     def _generate_plot_frame(self):
-        fig = Figure()
+        fig = Figure(figsize=(1,1))
         t = np.arange(0, 3, .01)
         ax = fig.gca() #fig.add_subplot(111)
-        ax.plot(t, 2 * np.sin(2 * np.pi * t))
+        # ax.plot(t, 2 * np.sin(2 * np.pi * t))
 
         canvas_frame = Frame(master=self.root)
 
         canvas = FigureCanvasTkAgg(fig, master = canvas_frame)
-        # canvas.draw()
         toolbar = NavigationToolbar2Tk(canvas, canvas_frame)
-        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1,anchor=tk.N)
         toolbar.update()
-        toolbar.pack_configure(side=tk.TOP, expand=True)
+        toolbar.grid_configure(row=0,column=0,sticky='nsew')
+        canvas.get_tk_widget().grid(row=1,column=0,sticky='nsew')
+
+        canvas_frame.rowconfigure(0, weight=0)
+        canvas_frame.rowconfigure(1, weight=1)
+        canvas_frame.columnconfigure(0, weight=1)
 
         return {'fig':fig, 'ax':ax, 'frame':canvas_frame, 'canvas':canvas, 'toolbar':toolbar}
+
+    def _event_plotsel_changed(self):
+        if self.plot_dim_type.get() == 1:
+            #1D Plot
+            self.lstbx_y.disable()
+            cur_height = self.pw_plots_main.winfo_height()
+            self.pw_plots_main.sash_place(0, 1, int(cur_height-1))
+        else:
+            #2D Plot
+            self.lstbx_y.enable()
+            cur_height = self.pw_plots_main.winfo_height()
+            self.pw_plots_main.sash_place(0, 1, int(cur_height*0.8))
 
 
     def _event_form_on_key_press(self,event):
@@ -90,3 +163,24 @@ class MainForm:
         root.destroy()  # this is necessary on Windows to prevent
                         # Fatal Python Error: PyEval_RestoreThread: NULL tstate
     
+class ListBoxScrollBar:
+    def __init__(self, parent_ui_element):
+        self.frame = Frame(master=parent_ui_element)
+
+        self.listbox = Listbox(self.frame)
+        self.listbox.pack(side = LEFT, fill = BOTH)
+        self.scrollbar = Scrollbar(self.frame)
+        self.scrollbar.pack(side = RIGHT, fill = BOTH)
+
+        for values in range(100): 
+            self.listbox.insert(END, values)
+        
+        self.listbox.config(yscrollcommand = self.scrollbar.set)
+        self.scrollbar.config(command = self.listbox.yview)
+
+    def enable(self):
+        self.listbox.configure(state='normal')
+        # self.scrollbar.configure(state='normal')
+    def disable(self):
+        self.listbox.configure(state='disabled')
+        # self.scrollbar.configure(state='disabled')
